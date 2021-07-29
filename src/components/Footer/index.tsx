11f7ea2d5c6450d773a29a0db100e7f81d300021
Nejc <nejcrogelsek0@gmail.com>
2021-07-29 20:56:20 +0200

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react'
+import LogoIconUrl from '../../assets/images/quotastic_logo_icon.svg'
 
 const Footer: FC = () => {
     return (
         <footer className="footer">
-            footer
+            <div className="container-fluid">
+                <img src={LogoIconUrl} alt='Quotastic' />
+                <p>All Rights Reserved | skillupmentor.com</p>
+            </div>
         </footer>
     )
 }
