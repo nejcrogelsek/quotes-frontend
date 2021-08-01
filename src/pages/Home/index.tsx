53557dc750e79fec5cfec339191e951232d225df
Nejc <nejcrogelsek0@gmail.com>
2021-08-01 22:12:53 +0200

    homepage explore title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,10 @@ import { Hero } from '../../components'
 
 const Home: FC = () => {
     return (
-        <div className="home-page">
-            <div className="container-lg">
+        <div className='home-page'>
+            <div className='container-lg'>
                 <Hero />
+                <h2 className='page-title explore-title'>Explore the world of <span>fantastic quotes</span></h2>
             </div>
         </div>
     )
