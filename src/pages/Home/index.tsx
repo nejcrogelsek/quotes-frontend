0fdfd9de03b7ee8fd21d1f9c0713b8895e4070e2
Nejc <nejcrogelsek0@gmail.com>
2021-08-03 19:49:35 +0200

    most liked quotes title + text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,13 +11,15 @@ const Home: FC = () => {
                         <h1 className='page-title text-center'><span>Quote of the day</span></h1>
                         <p className='site-text'>Quote of the day is randomly choosen quote.</p>
                         <QuoteBlock />
+                        <h2 className='page-title secondary most-liked-title'>Most liked quotes</h2>
+                        <p className='most-liked-text'>Most liked quotes on the platform.  Sign up or login to like the quotes  and keep them saved in your profile</p>
                     </>
                     :
                     <>
                         <Hero />
+                        <h2 className='page-title explore-title'>Explore the world of <span>fantastic quotes</span></h2>
                     </>
                 }
-                <h2 className='page-title explore-title'>Explore the world of <span>fantastic quotes</span></h2>
             </div>
         </div>
     )
