d7242e232bd4698bbb0c0b937ba83c1a7e196cfa
Nejc <nejcrogelsek0@gmail.com>
2021-08-10 16:40:41 +0200

    loading screen

---
+++ src/components/LoadingScreen/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+const LoadingScreen: FC = () => {
+    return (
+        <div className="loading-screen">
+            <h1>Redirecting...</h1>
+        </div>
+    )
+}
+
+export default LoadingScreen
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './style/css/main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LoadingScreen } from './components';
+
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<LoadingScreen />}><App /></Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
