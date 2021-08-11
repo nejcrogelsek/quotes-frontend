84d51a2cbe89207e3e1bfe1191c8bb118166d163
Nejc <nejcrogelsek0@gmail.com>
2021-08-11 14:14:01 +0200

    send data to backend

--- src/components/Forms/RegisterForm/index.tsx
+++ src/components/Forms/RegisterForm/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useLocation } from 'react-router-dom';
+import axios from '../../../axios';
 
 export type SignUpData = {
     email: string;
-    firstName: string;
-    lastName: string;
+    first_name: string;
+    last_name: string;
     password: string;
-    confirmPassword: string;
+    confirm_password: string;
 }
 
 const RegisterForm: FC = () => {
@@ -19,11 +20,14 @@ const RegisterForm: FC = () => {
         reset();
     })
 
-    const signin = async (data: SignUpData) => {
+    const signin = async (createUserDto: SignUpData) => {
         try {
-            alert(data);
+            console.log(createUserDto);
+            await axios.post('/users/create', createUserDto).then((res) => {
+                console.log(res.data);
+            });
         } catch (err) {
-            console.log('Error message:', err);
+            console.log('ERROR MESSAGE:', err);
         }
     }
 
@@ -37,16 +41,16 @@ const RegisterForm: FC = () => {
             <div className="row">
                 <div className="col">
                     <div className='form-element'>
-                        <label htmlFor='firstName' className='form-label'>First name</label>
-                        <input {...register('firstName', { required: 'First name is required' })} type='text' name='firstName' className='form-control' />
-                        {errors.firstName && <span className='form-text required'>{errors.firstName.message}</span>}
+                        <label htmlFor='first_name' className='form-label'>First name</label>
+                        <input {...register('first_name', { required: 'First name is required' })} type='text' name='first_name' className='form-control' />
+                        {errors.first_name && <span className='form-text required'>{errors.first_name.message}</span>}
                     </div>
                 </div>
                 <div className="col">
                     <div className='form-element'>
-                        <label htmlFor='lastName' className='form-label'>Last name</label>
-                        <input {...register('lastName', { required: 'Last name is required' })} type='text' name='lastName' className='form-control' />
-                        {errors.lastName && <span className='form-text required'>{errors.lastName.message}</span>}
+                        <label htmlFor='last_name' className='form-label'>Last name</label>
+                        <input {...register('last_name', { required: 'Last name is required' })} type='text' name='last_name' className='form-control' />
+                        {errors.last_name && <span className='form-text required'>{errors.last_name.message}</span>}
                     </div>
                 </div>
             </div>
@@ -56,9 +60,9 @@ const RegisterForm: FC = () => {
                 {errors.password && <span className='form-text required'>{errors.password.message}</span>}
             </div>
             <div className='form-element'>
-                <label htmlFor='confirmPassword' className='form-label'>Confirm password</label>
-                <input {...register('confirmPassword', { required: 'Please confirm password' })} type='text' name='confirmPassword' className='form-control' />
-                {errors.confirmPassword && <span className='form-text required'>{errors.confirmPassword.message}</span>}
+                <label htmlFor='confirm_password' className='form-label'>Confirm password</label>
+                <input {...register('confirm_password', { required: 'Please confirm password' })} type='text' name='confirm_password' className='form-control' />
+                {errors.confirm_password && <span className='form-text required'>{errors.confirm_password.message}</span>}
             </div>
             {location.pathname.slice(1, location.pathname.length) === 'signup' ?
                 <>
