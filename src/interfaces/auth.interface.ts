2b4586d71bc8b98ce7b3da8f8134f0d9905ccdac
Nejc <nejcrogelsek0@gmail.com>
2021-08-12 14:28:10 +0200

    folder structure

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,12 @@
+export type SignUpData = {
+    email: string;
+    first_name: string;
+    last_name: string;
+    password: string;
+    confirm_password: string;
+}
+
+export type SignInData = {
+    email: string;
+    password: string;
+}
