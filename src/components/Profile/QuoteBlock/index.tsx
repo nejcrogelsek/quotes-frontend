8efc83b31742e374fec5206c33f17449e9b0243d
Nejc <nejcrogelsek0@gmail.com>
2021-08-19 14:48:55 +0200

    remove quote

--- src/components/Profile/QuoteBlock/index.tsx
+++ src/components/Profile/QuoteBlock/index.tsx
@@ -1,9 +1,25 @@
 import { Avatar } from '@material-ui/core'
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import { AppProps } from '../../../interfaces/app.interface'
 import { Add } from '@material-ui/icons'
+import { QuoteContext } from '../../../stores/quote.context'
 
 const ProfileQuoteBlock: FC<AppProps> = ({ user, quote }: AppProps) => {
+    const { quoteValue, setQuoteValue } = useContext(QuoteContext);
+
+    const removeQuote = async () => {
+        const { id, votes, user_id, created_at, updated_at } = quoteValue;
+        console.log(id);
+        await setQuoteValue({
+            id,
+            votes,
+            message: '',
+            user_id,
+            created_at,
+            updated_at
+        })
+    }
+
     return (
         <div className='quote-block'>
             {quote && quote.message === '' ?
@@ -30,7 +46,7 @@ const ProfileQuoteBlock: FC<AppProps> = ({ user, quote }: AppProps) => {
                             <span className='quote-username'>{user.first_name}{' '}{user.last_name}</span>
                         </div>
                     </div>
-                    <button className='remove-quote'>
+                    <button className='remove-quote' onClick={removeQuote}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                         </svg>
