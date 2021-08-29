1523ecf8499dacf7543e240d412f8f8b65693e9e
Nejc <nejcrogelsek0@gmail.com>
2021-08-29 19:13:55 +0200

    display most recent quotes

--- src/components/Homepage/MostRecent/index.tsx
+++ src/components/Homepage/MostRecent/index.tsx
@@ -1,10 +1,14 @@
+import axios from '../../../api/axios';
 import { FC, useEffect, useState } from 'react'
 import { QuoteBlock } from '../..'
 import { QuoteData } from '../../../interfaces/quote.interface';
+import { useContext } from 'react';
+import { QuoteContext } from '../../../stores/quote.context';
 
 const MostRecent: FC = () => {
     const [isMobile, setIsMobile] = useState(true);
     const [stateQuotes, setStateQuotes] = useState<QuoteData[]>([]);
+    const { quoteValue } = useContext(QuoteContext);
 
     const checkIfMobile = () => {
         if (window.innerWidth < 992) {
@@ -14,13 +18,25 @@ const MostRecent: FC = () => {
         }
     };
 
+    const getRecentQuotes = async () => {
+        await axios.get('quotes/recent').then((res) => {
+            console.log(res.data);
+            setStateQuotes(res.data);
+        })
+    }
+
     useEffect(() => {
+        getRecentQuotes()
         checkIfMobile();
         window.addEventListener('resize', checkIfMobile);
         return () => {
             window.removeEventListener('resize', checkIfMobile);
         };
     }, []);
+
+    useEffect(() => {
+        getRecentQuotes();
+    }, [quoteValue])
 
     let loop: number = 0;
 
@@ -32,9 +48,17 @@ const MostRecent: FC = () => {
                 {stateQuotes.map(quote => {
                     loop++;
                     if (isMobile === true && loop < 5) {
-                        return null
+                        if (quote.message === '') {
+                            return null
+                        } else {
+                            return <QuoteBlock key={quote.id} id={quote.id} votes={quote.votes} message={quote.message} user={quote.user} />
+                        }
                     } else if (isMobile === false && loop < 10) {
-                        return null
+                        if (quote.message === '') {
+                            return null
+                        } else {
+                            return <QuoteBlock key={quote.id} id={quote.id} votes={quote.votes} message={quote.message} user={quote.user} />
+                        }
                     } else {
                         return null
                     }
