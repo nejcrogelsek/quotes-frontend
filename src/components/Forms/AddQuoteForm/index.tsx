d57d3b9e23859ac5d2acce2543cc28a3c9756ffb
Nejc <nejcrogelsek0@gmail.com>
2021-08-29 19:27:24 +0200

    default value for quote message

--- src/components/Forms/AddQuoteForm/index.tsx
+++ src/components/Forms/AddQuoteForm/index.tsx
@@ -1,16 +1,17 @@
 import axios from '../../../api/axios';
-import { FC, useContext } from 'react';
+import { FC, useContext, useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { AddQuoteData } from '../../../interfaces/quote.interface';
 import { UserContext } from '../../../stores/user.context';
 import { QuoteContext } from '../../../stores/quote.context';
 
 const AddQuoteForm: FC = () => {
+    const [defaultMessageValue, setDefaultMessageValue] = useState<string>('');
     const { userValue } = useContext(UserContext);
     const { quoteValue, setQuoteValue } = useContext(QuoteContext);
     const { register, handleSubmit, formState: { errors }, reset } = useForm<AddQuoteData>({
         defaultValues: {
-            message: quoteValue ? quoteValue.message : ''
+            message: defaultMessageValue
         }
     });
 
@@ -33,10 +34,16 @@ const AddQuoteForm: FC = () => {
         }
     }
 
+    useEffect(() => {
+        if (quoteValue) {
+            setDefaultMessageValue(quoteValue.message)
+        }
+    }, [quoteValue])
+
     return (
         <form onSubmit={onSubmit} className='form'>
             <div className='form-element textarea'>
-                <textarea {...register('message', { required: 'Quote is required' })} name='message' className='form-control' ></textarea>
+                <textarea {...register('message', { required: 'Quote is required' })} name='message' className='form-control' value={defaultMessageValue} onChange={e => setDefaultMessageValue(e.target.value)} ></textarea>
                 {errors.message && <span className='form-text required'>{errors.message.message}</span>}
             </div>
             <div className='buttons'>
